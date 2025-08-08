616e39dce648d530119afca535dc7d29a6b93bfc
Diksha <dikshakene06@gmail.com>
2025-08-08 10:56:40 +0530

    Add files via upload

---
+++ FACTORIAL P3.2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, i = 1;
+    unsigned long long fact = 1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num < 0)
+        printf("Factorial is not defined for negative numbers.");
+    else {
+        while (i <= num) {
+            fact *= i;
+            i++;
+        }
+        printf("Factorial of %d = %llu", num, fact);
+    }
+    return 0;
+}
