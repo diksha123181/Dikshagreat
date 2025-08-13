d48a10e31cf53fb5fcc40a11c5e2f8a85d62b28c
Diksha <dikshakene06@gmail.com>
2025-08-13 14:53:24 +0530

    Add files via upload

---
+++ square calculator p4.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int area(int side) {
+return side * side;
+}
+int main()
+{
+    int side;
+    printf("Enter the side of the square: ");
+    scanf("%d", &side);
+    printf("Area of square = %d", area(side));
+
+    return 0;
+}
